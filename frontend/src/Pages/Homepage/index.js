ebe5b00dc35e20f11169e05e6c224a2acc0725e7
Moontoast435 <seanmalone1993@gmail.com>
2022-10-04 12:18:09 +0100

    style homepage

--- frontend/src/Pages/Homepage/index.js
+++ frontend/src/Pages/Homepage/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './homepage.css';
 
 const HomePage = () => {
 let navigate = useNavigate();
 
     return (
         <div>
-            home lol
-        <button onClick={() => navigate('./game')}>Play Game</button>
+            <h1 className="title">Shooting Range</h1>
+            <div className="menu-wrapper">
+                <div className="menu">
+                <button className="menu-buttons" onClick={() => navigate('./game')}>Play Game</button>
+                <button className="menu-buttons">How to play</button>
+                <button className="menu-buttons">Options</button>
+                <button className="menu-buttons">Leaderboard</button>
+            </div>
+            </div>
         </div>
     );
 }
